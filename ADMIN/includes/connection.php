ccb68ae5f2a7d7882af6fd89f207a37c0f1c45ae
John Anthony Paquiz <johnanthonypaquiz17@gmail.com>
2022-03-07 15:31:12 +0800

    updatedb

--- ADMIN/includes/connection.php
+++ ADMIN/includes/connection.php
@@ -1,7 +1,7 @@
 <?php
 		
 	// $conn = mysqli_connect("localhost", "root", "", "capstone");
-	$conn = mysqli_connect("capstone-do-user-11002452-0.b.db.ondigitalocean.com", "doadmin", "5J2OAInFHtaHHmhz", "defaultdb", "25060");
+	$conn = mysqli_connect("capstone-do-user-11002452-0.b.db.ondigitalocean.com", "doadmin", "hNKv9CmkpvobFXT7", "defaultdb", "25060");
 	
 	if(!$conn){
 		die("Error: Failed to connect to database!");
