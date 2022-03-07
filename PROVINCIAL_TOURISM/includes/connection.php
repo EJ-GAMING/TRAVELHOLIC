92139da654dc1307b0edd06d5f1f34a6e5cdd839
John Anthony Paquiz <johnanthonypaquiz17@gmail.com>
2022-03-07 15:21:01 +0800

    update

--- PROVINCIAL_TOURISM/includes/connection.php
+++ PROVINCIAL_TOURISM/includes/connection.php
@@ -1,8 +1,12 @@
 <?php
 		
-	$conn = mysqli_connect("localhost", "root", "", "capstone");
+	// $conn = mysqli_connect("localhost", "root", "", "capstone");
+	$conn = mysqli_connect("capstone-do-user-11002452-0.b.db.ondigitalocean.com", "doadmin", "5J2OAInFHtaHHmhz", "defaultdb", "25060");
 	
 	if(!$conn){
 		die("Error: Failed to connect to database!");
 	}
-?>
+
+
+
+   ?>
