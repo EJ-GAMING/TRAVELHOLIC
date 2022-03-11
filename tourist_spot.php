502e2bfc152cd031b4acd1575d3a2f4ceded6997
John Anthony Paquiz <johnanthonypaquiz17@gmail.com>
2022-03-11 13:04:37 +0800

    update

--- tourist_spot.php
+++ tourist_spot.php
@@ -156,8 +156,8 @@ function selectRating($tsm_id, $conn)
     <?php include_once 'footer.php'; ?>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script>
-    <script src="js/bootstrap.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/script.js"></script>
 
 
