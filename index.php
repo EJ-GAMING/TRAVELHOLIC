1496520a837cddf61b84347fadb5749b1b8be367
John Anthony Paquiz <johnanthonypaquiz17@gmail.com>
2022-03-11 09:12:31 +0800

    update

--- index.php
+++ index.php
@@ -28,24 +28,7 @@ $rowcount =  mysqli_num_rows($result);
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
     <link rel="stylesheet" href="css/bootstrap.reboot.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
-    <script type="text/javascript"> 
-function disableselect(e){  
-return false  
-}  
-
-function reEnable(){  
-return true  
-}  
-
-//if IE4+  
-document.onselectstart=new Function ("return false")  
-document.oncontextmenu=new Function ("return false")  
-//if NS6  
-if (window.sidebar){  
-document.onmousedown=disableselect  
-document.onclick=reEnable  
-}
-</script>
+  
 </head>
 <body>
 <?php include_once 'navbar.php'?>
